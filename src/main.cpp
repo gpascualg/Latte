9b6c18f2366652178fe8fa0853b8185f525790f1
gpascualg <gpascualg93@gmail.com>
2016-02-09 12:31:50 +0100

    Added Graph for easy stacking

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <gflags/gflags.h>
 #include <iostream>
+#include <vector>
 
 #include "matrix.hpp"
 
@@ -23,6 +24,12 @@ public:
 		{
 			(**_weights)[i] = DType(rand()) / RAND_MAX - DType(0.5);
 		}
+
+		std::cout << "Setting up:" << std::endl;
+		std::cout << "Indata shape: (" << shape.m << ", " << shape.n << ")" << std::endl;
+		std::cout << "Weight shape: (" << _weights->shape().m << ", " << _weights->shape().n << ")" << std::endl;
+		std::cout << "Output shape: (" << _output->shape().m << ", " << _output->shape().n << ")" << std::endl;
+		std::cout << "--------" << std::endl << std::endl;
 	}
 
 	virtual Matrix<DType>* forward(Matrix<DType>* in) = 0;
@@ -49,11 +56,7 @@ class Sigmoid : public Layer<DType>
 public:
 	Sigmoid(Shape shape, int num_output) :
 		Layer(shape, num_output)
-	{
-		std::cout << "In shape: (" << shape.m << ", " << shape.n << ")" << std::endl;
-		std::cout << "We shape: (" << _weights->shape().m << ", " << _weights->shape().n << ")" << std::endl;
-		std::cout << "Ou shape: (" << _output->shape().m << ", " << _output->shape().n << ")" << std::endl;
-	}
+	{}
 
 	Matrix<DType>* forward(Matrix<DType>* in)
 	{
@@ -83,9 +86,107 @@ public:
 	}
 };
 
+template <typename DType>
+class Graph
+{
+public:
+	Graph(Matrix<DType>* target, Matrix<DType>* data):
+		_target(target),
+		_first_in(data),
+		_k(0)
+	{}
+
+	void stack(Layer<DType>* layer)
+	{
+		_layers.push_back(layer);
+	}
+
+	template <typename LType>
+	void stack(int num_output)
+	{
+		LType* layer = nullptr;
+
+		if (_layers.empty())
+		{
+			layer = new LType(_first_in->shape(), num_output);
+		}
+		else
+		{
+			layer = new LType(_layers.back()->outShape(), num_output);
+		}
+
+		stack(layer);
+	}
+
+	void forward()
+	{
+		Matrix<DType>* in = _first_in;
+
+		auto it = _layers.begin();
+		for (; it != _layers.end(); ++it)
+		{
+			in = (*it)->forward(in);
+		}
+
+		_last_out = in;
+	}
+
+	void backward()
+	{
+		// Initial error
+		Matrix<DT>* error = (*_target - *_last_out);
+
+		if ((_k % 10000) == 0)
+		{
+			DT sum = DT(0.0);
+			for (int i = 0; i < error->shape().prod(); ++i)
+				sum += abs((**error)[i]);
+
+			std::cout << "ERROR: " << (sum / error->shape().prod()) << std::endl;
+			std::cout << (*_target)(0, 0) << "\t" << (*_target)(1, 0) << "\t" << (*_target)(2, 0) << "\t" << (*_target)(3, 0) << std::endl;
+			std::cout << (*_last_out)(0, 0) << "\t" << (*_last_out)(1, 0) << "\t" << (*_last_out)(2, 0) << "\t" << (*_last_out)(3, 0) << std::endl << std::endl;
+		}
+		++_k;
+
+		// Skip last, it will be manually computed
+		auto last_layer = _layers.back();
+		auto it = ++_layers.rbegin();
+
+		error = last_layer->backward(error);
+
+		for (; it != _layers.rend(); ++it)
+		{
+			Matrix<DT>* delta = MatrixFactory<DT>::get()->pop({ last_layer->outShape().m, last_layer->W()->shape().m });
+
+			error->mul(&last_layer->W()->T(), delta);
+			last_layer = *it;
+			error = last_layer->backward(delta);
+		}
+
+		// Update layers
+		it = _layers.rbegin();
+		for (; it != _layers.rend(); ++it)
+		{
+			(*it)->update();
+		}
+
+		// Update Matrix pool
+		MatrixFactory<DT>::get()->update();
+	}
+
+
+private:
+	int _k;
+	std::vector<Layer<DType>* > _layers;
+	Matrix<DType>* _target;
+	Matrix<DType>* _first_in;
+	Matrix<DType>* _last_out;
+};
+
 int main(int argc, char** argv)
 {
 	gflags::ParseCommandLineFlags(&argc, &argv, true);
+	srand(time(NULL));
 
 	Matrix<DT> x(4, 3);
 	x(0, 0) = 0; x(0, 1) = 0; x(0, 2) = 1;
@@ -96,44 +197,16 @@ int main(int argc, char** argv)
 	Matrix<DT> y(4, 1);
 	y(0, 0) = 0; y(1, 0) = 1; y(2, 0) = 1; y(3, 0) = 0;
 	
-	srand(time(NULL));
-	Sigmoid<DT>* sigmoid_1 = new Sigmoid<DT>(x.shape(), 50);
-	Sigmoid<DT>* sigmoid_2 = new Sigmoid<DT>(sigmoid_1->outShape(), 1);
+	Graph<DT> graph(&y, &x);
+	graph.stack<Sigmoid<DT>>(50);
+	graph.stack<Sigmoid<DT>>(50);
+	graph.stack<Sigmoid<DT>>(50);
+	graph.stack<Sigmoid<DT>>(1);
 	
 	for (int k = 0; k < 600000; ++k)
 	{
-		// Forward
-		Matrix<DT>* L1 = sigmoid_1->forward(&x);
-		Matrix<DT>* L2 = sigmoid_2->forward(L1);
-
-		// Backward L2
-		Matrix<DT>* L2_error = y - *L2;
-
-		if ((k % 10000) == 0)
-		{
-			DT sum = DT(0.0);
-			for (int i = 0; i < L2_error->shape().prod(); ++i)
-				sum += abs((**L2_error)[i]);
-
-			std::cout << "ERROR: " << (sum / L2_error->shape().prod()) << std::endl;
-			std::cout << (y)(0, 0) << "\t" << (y)(1, 0) << "\t" << (y)(2, 0) << "\t" << (y)(3, 0) << std::endl;
-			std::cout << (*L2)(0, 0) << "\t" << (*L2)(1, 0) << "\t" << (*L2)(2, 0) << "\t" << (*L2)(3, 0) << std::endl << std::endl;
-		}
-
-		Matrix<DT>* L2_delta = sigmoid_2->backward(L2_error);
-
-		// Backward L1
-		Matrix<DT>* L1_error = MatrixFactory<DT>::get()->pop({ sigmoid_2->outShape().m, sigmoid_2->W()->shape().m });
-
-		L2_delta->mul(&sigmoid_2->W()->T(), L1_error);
-		Matrix<DT>* L1_delta = sigmoid_1->backward(L1_error);
-
-		// Update layers
-		sigmoid_2->update();
-		sigmoid_1->update();
-
-		// Update Matrix pool
-		MatrixFactory<DT>::get()->update();
+		graph.forward();
+		graph.backward();
 	}
 
 	getchar();
